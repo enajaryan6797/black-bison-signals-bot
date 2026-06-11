d3733369175853998adc19397907a3430b106df2
enajaryan6797 <enajaryan@gmail.com>
2026-06-11 01:09:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,8 +51,8 @@ def send_scan():
         text = "🐃 BLACK BISON KRAKEN SCANNER\n\n"
 
         for pair, price, change, volume in signals:
-    pretty_pair = pair.replace("USD", "/USD")
-    text += f"📈 Pair: {pretty_pair}\n"
+            pretty_pair = pair.replace("USD", "/USD")
+            text += f"📈 Pair: {pretty_pair}\n"
             text += f"💰 Price: {price}\n"
             text += f"🔥 Change: +{change:.2f}%\n"
             text += f"📊 Volume: ${volume:,.0f}\n"
